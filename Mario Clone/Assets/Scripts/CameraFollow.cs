275f7147b093b8bbb543fb312942a4c295e568c9
Vinicius Civali <viniciuscivali@gmail.com>
2021-08-07 18:50:13 -0300

    All the base game

---
+++ Mario Clone/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform leftBounds;
+    public Transform rightBounds;
+
+    public float smoothDampTime = 0.15f;
+    private Vector3 smoothDampVelocity = Vector3.zero;
+
+    private float camWidth, camHeight, levelMinX, levelMaxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+
+        float leftBoundWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        float rightBoundWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+
+        levelMinX = leftBounds.position.x + leftBoundWidth + (camWidth / 2);
+        levelMaxX = rightBounds.position.x - rightBoundWidth - (camWidth / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x) );
+
+            float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);
+
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+        }
+    }
+}
